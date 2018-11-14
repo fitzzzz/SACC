fc539d51442295d6f20cae295b21f30e2bbf13b8
david lang <david.lang697@gmail.com>
2018-11-14 23:24:16 +0100

    Ajout de la route de download

--- src/src/main/java/fr/si5/cc/td1/users/DecrementUserCurrentUsageTask.java
+++ src/src/main/java/fr/si5/cc/td1/users/DecrementUserCurrentUsageTask.java
@@ -1,4 +1,22 @@
 package fr.si5.cc.td1.users;
 
-public class DecrementUserCurrentUsageTask {
+import com.google.appengine.api.taskqueue.DeferredTask;
+
+public class DecrementUserCurrentUsageTask implements DeferredTask {
+
+    private final String userId;
+
+    public DecrementUserCurrentUsageTask(String userId) {
+        this.userId = userId;
+    }
+
+    @Override
+    public void run() {
+        UserDao userDao = new UserDao();
+        User userSynchronized = userDao.getUserByLogin(userId);
+        System.out.println("[DecrementUserCurrentUsageTask] user : " + userId
+                + "new current usage : " + userSynchronized.decrementCurrentUsage());
+        userDao.updateEntity(userSynchronized);
+    }
+
 }
