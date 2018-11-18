159a14f6167ca5c9bd5627864ae72f0e82329c92
Alexandre HILTCHER <alex.hiltcher@gmail.com>
2018-11-18 12:57:22 +0100

    Add userName to create FakeFile request

--- src/src/main/java/fr/si5/cc/td1/files/CreateFakeFileServlet.java
+++ src/src/main/java/fr/si5/cc/td1/files/CreateFakeFileServlet.java
@@ -9,16 +9,22 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-    @WebServlet(name = "CreatFakeFile", value = "/createFakeFile")
+@WebServlet(name = "CreatFakeFile", value = "/createFakeFile")
 public class CreateFakeFileServlet extends HttpServlet {
     private FileStorage fileStorage = FileStorage.getInstance();
+    private FileDao fileDao = new FileDao();
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String fileName = req.getParameter("fileName");
         int size = Integer.parseInt(req.getParameter("size"));
+        String userName = req.getParameter("userName");
+
         BlobInfo info = fileStorage.createFakeFile(fileName, size);
-        resp.getWriter().print(info.getMediaLink());
+
+        fileDao.save(new File(userName, info.getName(), info.getMediaLink(), info.getName()));
+
+        resp.getWriter().print("http://projet-sacc-si5.appspot.com/download?fileName=" + info.getName() + "&userMail=");
 
     }
 }
